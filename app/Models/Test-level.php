13c62ac90edc3398b99732042cbc8f388706fdc1
Thibault Conte <thibault.conte@ynov.com>
2021-05-23 17:50:51 +0200

    [API] API ressources deployement

--- app/Models/Test-level.php
+++ app/Models/Test-level.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Test-level extends Model
+class Testlevel extends Model
 {
     use HasFactory;
 
